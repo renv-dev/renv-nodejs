4bd24c60e0eeddc83302e59bcd09566f33cb9ffc
tanahiro2010 <herentongkegu087@gmail.com>
2026-01-07 13:52:23 +0900

    init: upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const { Renv } = require('../../dist');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+
+const TOKEN = process.env.TOKEN;
+console.log(`Using TOKEN: ${TOKEN}`);
+
+(async () => {
+    const client = new Renv(TOKEN, true);
+    await client.load();
+})();
